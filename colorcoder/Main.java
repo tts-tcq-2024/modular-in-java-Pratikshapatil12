6f94e2d73cdf5a3381cea44653d9b853927247d7
Pratikshapatil12 <100845044+Pratikshapatil12@users.noreply.github.com>
2024-08-04 17:24:58 +0530

    Update Main.java

--- colorcoder/Main.java
+++ colorcoder/Main.java
@@ -1,15 +1,17 @@
 package colorcoder;
 
 public class Main {
-
     public static void main(String[] args) {
-        ColorUtil.testNumberToPair(4, MajorColor.WHITE, MinorColor.BROWN);
-        ColorUtil.testNumberToPair(5, MajorColor.WHITE, MinorColor.SLATE);
+        // Test color pair conversions
+        ColorUtilTest.testNumberToPair(4, MajorColor.WHITE, MinorColor.BROWN);
+        ColorUtilTest.testNumberToPair(5, MajorColor.WHITE, MinorColor.SLATE);
 
-        ColorUtil.testPairToNumber(MajorColor.BLACK, MinorColor.ORANGE, 12);
-        ColorUtil.testPairToNumber(MajorColor.VIOLET, MinorColor.SLATE, 25);
+        ColorUtilTest.testPairToNumber(MajorColor.BLACK, MinorColor.ORANGE, 12);
+        ColorUtilTest.testPairToNumber(MajorColor.VIOLET, MinorColor.SLATE, 25);
 
+        // Print the color coding reference manual
         System.out.println("Color Coding Reference Manual:\n");
         System.out.println(ColorUtil.getColorManual());
     }
 }
+
