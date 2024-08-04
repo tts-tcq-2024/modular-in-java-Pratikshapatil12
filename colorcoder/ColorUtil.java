54662e899000ade8a82ea5ba4f96999ed81d989d
Pratikshapatil12 <100845044+Pratikshapatil12@users.noreply.github.com>
2024-08-04 17:24:01 +0530

    Update ColorUtil.java

--- colorcoder/ColorUtil.java
+++ colorcoder/ColorUtil.java
@@ -1,4 +1,5 @@
 package colorcoder;
+
 public class ColorUtil {
     public final static String MajorColorNames[] = {
         "White", "Red", "Black", "Yellow", "Violet"
@@ -6,6 +7,7 @@ public class ColorUtil {
     public final static String MinorColorNames[] = {
         "Blue", "Orange", "Green", "Brown", "Slate"
     };
+
     public static ColorPair GetColorFromPairNumber(int pairNumber) {
         int zeroBasedPairNumber = pairNumber - 1;
         MajorColor majorColor = MajorColor.fromIndex(zeroBasedPairNumber / MinorColor.values().length);
@@ -16,21 +18,7 @@ public class ColorUtil {
     public static int GetPairNumberFromColor(MajorColor major, MinorColor minor) {
         return major.getIndex() * MinorColor.values().length + minor.getIndex() + 1;
     }
-    public static void testNumberToPair(int pairNumber,
-                                        MajorColor expectedMajor,
-                                        MinorColor expectedMinor) {
-        ColorPair colorPair = GetColorFromPairNumber(pairNumber);
-        System.out.println("Got pair " + colorPair.toString());
-        assert(colorPair.getMajor() == expectedMajor);
-        assert(colorPair.getMinor() == expectedMinor);
-    }
-    public static void testPairToNumber(MajorColor major,
-                                        MinorColor minor,
-                                        int expectedPairNumber) {
-        int pairNumber = GetPairNumberFromColor(major, minor);
-        System.out.println("Got pair number " + pairNumber);
-        assert(pairNumber == expectedPairNumber);
-    }
+
     public static String getColorManual() {
         StringBuilder manual = new StringBuilder();
         for (int i = 1; i <= MajorColor.values().length; i++) {
